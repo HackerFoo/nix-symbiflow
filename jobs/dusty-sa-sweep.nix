3c9c53ec8f3ebcaf8083d8644e4de8a9f0591bc9
Dusty DeWeese <dustin.deweese@gmail.com>
2020-07-10 13:49:08 -0700

    more values

--- jobs/dusty-sa-sweep.nix
+++ jobs/dusty-sa-sweep.nix
@@ -8,13 +8,15 @@ with callPackage ../library.nix {};
 
 let
   params_list =
-    attr_sweep {
-      alpha_min = [0.7 0.8 0.85];
-      alpha_max = [0.86 0.9];
-      alpha_decay = [0.55 0.5 0.45 0.4];
-      anneal_success_target = [0.5 0.55 0.6 0.65];
-      anneal_success_min = [0.1 0.15];
-    };
+    filter
+      (x: (x.alpha_min < x.alpha_max) && (x.anneal_success_min < x.anneal_success_target))
+      (attr_sweep {
+        alpha_min = [0.6 0.7 0.8 0.85];
+        alpha_max = [0.7 0.8 0.86 0.9];
+        alpha_decay = [0.55 0.5 0.45 0.4 0.3];
+        anneal_success_target = [0.5 0.55 0.6 0.65 0.7];
+        anneal_success_min = [0.1 0.15 0.18];
+      });
 in
 
 listToAttrs (map (params: {
