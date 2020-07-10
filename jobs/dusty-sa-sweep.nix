eacdd0de0bdca1384df77c01caf322c6ed6601ed
Dusty DeWeese <dustin.deweese@gmail.com>
2020-07-10 15:02:06 -0700

    more values

--- jobs/dusty-sa-sweep.nix
+++ jobs/dusty-sa-sweep.nix
@@ -11,11 +11,11 @@ let
     filter
       (x: (x.alpha_min < x.alpha_max) && (x.anneal_success_min < x.anneal_success_target))
       (attr_sweep {
-        alpha_min = [0.6 0.7 0.8 0.85];
-        alpha_max = [0.7 0.8 0.86 0.9];
-        alpha_decay = [0.55 0.5 0.45 0.4 0.3];
-        anneal_success_target = [0.5 0.55 0.6 0.65 0.7];
-        anneal_success_min = [0.1 0.15 0.18 0.25 0.4 0.5];
+        alpha_min = [0.75 0.8 0.82];
+        alpha_max = [0.88 0.9 0.92];
+        alpha_decay = [0.42 0.4 0.38];
+        anneal_success_target = [0.58 0.6 0.62];
+        anneal_success_min = [0.17 0.18 0.19 0.2];
       });
 in
 
