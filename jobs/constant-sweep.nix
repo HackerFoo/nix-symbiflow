946f9bb5c9800bfed2cf971c1afb75675bb24621
Dusty DeWeese <dustin.deweese@gmail.com>
2020-10-29 10:30:25 -0700

    update params

--- jobs/constant-sweep.nix
+++ jobs/constant-sweep.nix
@@ -9,8 +9,8 @@ with callPackage ../library.nix {};
 
 let
   params_list = attr_sweep {
-    kIncreaseFocusLimit = [ 7 17 31 61 127 253 256 511 512 1021 1543 2039 2048 ];
-    kScale = [ 1.4 1.7 2 2.5 3 5 ]; # 3
+    kIncreaseFocusLimit = [ 2011 2017 2027 2029 2039 2048 2053 2063 2069 2081 2083 ];
+    kScale = [ 1.4 1.7 2 2.2 2.5 3 5 ]; # 2, 3
   };
 in
 
