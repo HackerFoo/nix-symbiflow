3d5814e5fb4fa9b41f5db41d824f009924beb33c
Dusty DeWeese <dustin.deweese@gmail.com>
2020-11-05 07:20:54 -0800

    update params, add bram-n3

--- jobs/param-sweep.nix
+++ jobs/param-sweep.nix
@@ -11,7 +11,7 @@ let
   params_list = attr_sweep {
     place_delay_model = [ "delta" "delta_override" ];
     initial_pres_fac = [ 2 2.828 4 ]; # 2.828
-    astar_fac = [ 1.5 1.6 1.8 2 ]; # 1.8
+    astar_fac = [ 1.2 1.5 1.6 1.8 2 ]; # 1.8
     first_iter_pres_fac = [ 0.5 ];
     pres_fac_mult = [ 1.1 1.15 1.2 1.25 1.3 ]; # 1.2
     acc_fac = [ 0.4 0.5 0.7 ]; # 0.7
@@ -25,4 +25,8 @@ listToAttrs ((map (params: {
 (map (params: {
   name = "ibex" + replaceStrings ["."] ["_"] (attrs_to_string "_" "_" params);
   value = (make-fpga-tool-perf { extra_vpr_flags = params; }).ibex.vpr.arty;
+}) params_list) ++
+(map (params: {
+  name = "bram-n3" + replaceStrings ["."] ["_"] (attrs_to_string "_" "_" params);
+  value = (make-fpga-tool-perf { extra_vpr_flags = params; }).bram-n3.vpr.arty;
 }) params_list))
