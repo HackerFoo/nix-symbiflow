1d9c32510bb99bcd4a0dd6c63267626e1535b378
Dusty DeWeese <dustin.deweese@gmail.com>
2020-12-16 07:07:29 -0800

    update sources, add symbiflow-arch-defs-install

---
+++ list-symbiflow-tarballs.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+URL_BASE=https://storage.googleapis.com/symbiflow-arch-defs-gha/
+declare -A PACKAGE HASH URL
+PACKAGE[benchmarks]=symbiflow-benchmarks-latest
+PACKAGE[toolchain]=symbiflow-toolchain-latest
+PACKAGE[arch_50t]=symbiflow-xc7a50t_test-latest
+PACKAGE[arch_100t]=symbiflow-xc7a100t_test-latest
+PACKAGE[arch_200t]=symbiflow-xc7a200t_test-latest
+
+for data in "${!PACKAGE[@]}"; do
+    URL[$data]=`curl -s ${URL_BASE}${PACKAGE[$data]}`
+    HASH[$data]=`nix-prefetch-url --name $data "${URL[$data]}"`
+done
+
+for data in "${!PACKAGE[@]}"; do
+    echo "    $data = fetchurl {"
+    echo "      name = \"$data\";"
+    echo "      url = \"${URL[$data]}\";"
+    echo "      sha256 = \"${HASH[$data]}\";"
+    echo "    };"
+done
+
