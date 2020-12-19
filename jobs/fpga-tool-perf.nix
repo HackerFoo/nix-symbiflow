2dc7d0455b571cf7d4f9ab5ac1c7b1eeddb01add
Dusty DeWeese <dustin.deweese@gmail.com>
2020-12-19 07:40:31 -0800

    Update parameters.

--- jobs/fpga-tool-perf.nix
+++ jobs/fpga-tool-perf.nix
@@ -7,14 +7,24 @@ with lib;
 with callPackage ../default.nix {};
 
 let
-  make_tests = fpga-tool-perf: suffix: concatMap (project:
+  make_tests = jobs: suffix: concatMap (project:
     concatMap (toolchain:
       map (board: {
         name = "${project}_${toolchain}_${board}" + suffix;
-        value = fpga-tool-perf.${project}.${toolchain}.${board};
-      }) (attrNames fpga-tool-perf.${project}.${toolchain}))
-      (attrNames fpga-tool-perf.${project}))
-    (attrNames fpga-tool-perf);
+        value = jobs.${project}.${toolchain}.${board};
+      }) (attrNames jobs.${project}.${toolchain}))
+      (attrNames jobs.${project}))
+    (attrNames jobs);
+  jobs = make-fpga-tool-perf {
+    extra_vpr_flags = {
+      acc_fac = 1.0;
+      astar_fac = 2.0;
+      first_iter_pres_fac = 0;
+      initial_pres_fac = 2.828;
+      place_delay_model = "delta_override";
+      pres_fac_mult = 1.2;
+    };
+  };
 in
 
-listToAttrs (make_tests fpga-tool-perf "")
+listToAttrs (make_tests jobs "")
